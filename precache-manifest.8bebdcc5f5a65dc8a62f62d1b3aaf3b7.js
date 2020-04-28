b84a518c9aa5ef9f0e3fa0ec0d88b51fbffcafbd
William Steck <steckwilliam@gmail.com>
2020-04-28 17:56:08 -0500

    Updates

---
+++ precache-manifest.8bebdcc5f5a65dc8a62f62d1b3aaf3b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dd18d0d1c680a7e1f7a817e23c2324",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31a731f21527cc33df2",
+    "url": "/static/css/main.7e9d6821.chunk.css"
+  },
+  {
+    "revision": "ba5898e718b5d2060cc6",
+    "url": "/static/js/2.c905932b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c905932b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31a731f21527cc33df2",
+    "url": "/static/js/main.d2d35196.chunk.js"
+  },
+  {
+    "revision": "b3b41027ca6c2eccc76b",
+    "url": "/static/js/runtime-main.ed2263ad.js"
+  },
+  {
+    "revision": "c82afa6689c6fb065fb29f6673858288",
+    "url": "/static/media/0.c82afa66.jpg"
+  },
+  {
+    "revision": "a6c4a57393b5e380827e0ee175262ef7",
+    "url": "/static/media/backend.a6c4a573.png"
+  },
+  {
+    "revision": "238dad299fc317baa566a0e316e006b5",
+    "url": "/static/media/front-end.238dad29.png"
+  },
+  {
+    "revision": "995d290deadef3a3f50d80c11aea6cb3",
+    "url": "/static/media/me-illu.995d290d.png"
+  },
+  {
+    "revision": "8d4ac6ddc458bf8ceebdea4f43a0d901",
+    "url": "/static/media/other.8d4ac6dd.png"
+  },
+  {
+    "revision": "f6868c7e95f9fdcbea5d9f3230b7141f",
+    "url": "/static/media/spotlotproject.f6868c7e.png"
+  }
+]);
